91ff0515ebf458f22f8f54051e109e652911038d
Jared C. Gober <jaredcgober@gmail.com>
2020-09-30 21:55:57 -0400

    refined tip calculator

--- public/js/script.js
+++ public/js/script.js
@@ -9,21 +9,26 @@ let total = $("#total")
 calc.on("click", function(){
 
     let billAmount =document.getElementById("billAmount").value
+        
+        billAmount = parseFloat(billAmount)
 
     let tipAmount = document.getElementById("tipAmount").value
 
-    let tip = parseFloat(billAmount) * parseFloat(tipAmount)
+        tipAmount = parseFloat(tipAmount)/100
+
+    let tip = billAmount * tipAmount
+        
+        tip.toFixed(2)
 
     let totalCost = parseFloat(billAmount) + tip
 
-
-    console.log(tip)
-
     let div = $("<div>")
 
-    div.html(`Bill: ${billAmount}, Tip $${tip}, Total: $${totalCost}`)
+    div.html(`<h3>Bill: $${billAmount.toFixed(2)}, Tip: $${tip.toFixed(2)}, Total: $${totalCost.toFixed(2)}</h3>`)
 
-    div.appendTo(total)
+    div.attr("style", "margin: 20px")
+
+    div.prependTo(total)
 
     console.log("Working")
 
