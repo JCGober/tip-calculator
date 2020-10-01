ef4f945c460aea286cac449beee8ce7872416f90
Jared C. Gober <jaredcgober@gmail.com>
2020-09-30 21:26:19 -0400

    Got tip portion of javascript working

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,30 @@
-let billAmount = ${"billAmount"}
+let billAmount = document.getElementById("billAmount").value
+
+let tipAmount = document.getElementById("tipAmount").value
+
+let calc = $("#calc")
+
+let total = $("#total")
+
+calc.on("click", function(){
+
+    let billAmount =document.getElementById("billAmount").value
+
+    let tipAmount = document.getElementById("tipAmount").value
+
+    let tip = parseFloat(billAmount) * parseFloat(tipAmount)
+
+    let totalCost = parseFloat(billAmount) + tip
+
+
+    console.log(tip)
+
+    let div = $("<div>")
+
+    div.html(`Bill: ${billAmount}, Tip $${tip}, Total: $${totalCost}`)
+
+    div.appendTo(total)
+
+    console.log("Working")
+
+});
